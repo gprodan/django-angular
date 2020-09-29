9412120aaa238d20f42365d821901346507009e3
gprodan <gprodan@univ-ovidius.ro>
2020-09-29 17:44:59 +0300

    v1.4

--- django/todoapi/views.py
+++ django/todoapi/views.py
@@ -16,30 +16,10 @@ class TaskList(generics.ListCreateAPIView):
     serializer_class = TaskSerializer
 
 
-class TaskDetail(APIView):
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Returns a single Task and allows updates and deletion of a Task.
     """
-    def get_object(self, task_id):
-        try:
-            return Task.objects.get(pk=task_id)
-        except Task.DoesNotExist:
-            raise Http404
-
-    def get(self, request, task_id, format=None):
-        task = self.get_object(task_id)
-        serializer = TaskSerializer(task)
-        return Response(serializer.data)
-
-    def put(self, request, task_id, format=None):
-        task = self.get_object(task_id)
-        serializer = TaskSerializer(task, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, task_id, format=None):
-        task = self.get_object(task_id)
-        task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_url_kwarg = 'task_id'
