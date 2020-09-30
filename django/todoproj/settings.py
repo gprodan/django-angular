87a72565a9a6e87327de8f88a25c15f2be5235ed
gprodan <gprodan@univ-ovidius.ro>
2020-09-30 11:33:56 +0300

    v1.10

--- django/todoproj/settings.py
+++ django/todoproj/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-# Imports for JWT authentication.
-import json
-from six.moves.urllib import request
-from cryptography.x509 import load_pem_x509_certificate
-from cryptography.hazmat.backends import default_backend
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_jwt',
     'todoapi',
     'corsheaders',
 ]
@@ -144,38 +138,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
 )
 
-## Add JWT authentication to default authentication classes
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated', # All views have this permission active (unless overwritten).
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    ),
-}
 
 
-AUTH0_DOMAIN = 'https://dev-5k75yktt.eu.auth0.com/'
-API_IDENTIFIER = 'http://djangoangularapi'
-PUBLIC_KEY = None
-JWT_ISSUER = None
 
-if AUTH0_DOMAIN:
-    jsonurl = request.urlopen('https://' + AUTH0_DOMAIN + '/.well-known/jwks.json')
-    jwks = json.loads(jsonurl.read().decode('utf-8'))
-    cert = '-----BEGIN CERTIFICATE-----\n' + jwks['keys'][0]['x5c'][0] + '\n-----END CERTIFICATE-----'
-    certificate = load_pem_x509_certificate(cert.encode('utf-8'), default_backend())
-    PUBLIC_KEY = certificate.public_key()
-    JWT_ISSUER = 'https://' + AUTH0_DOMAIN + '/'
-
-def jwt_get_username_from_payload_handler(payload):
-    return 'auth0user'
-
-JWT_AUTH = {
-    'JWT_PAYLOAD_GET_USERNAME_HANDLER': jwt_get_username_from_payload_handler,
-    'JWT_PUBLIC_KEY': PUBLIC_KEY,
-    'JWT_ALGORITHM': 'RS256',
-    'JWT_AUDIENCE': API_IDENTIFIER,
-    'JWT_ISSUER': JWT_ISSUER,
-    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
-}   
+ 
