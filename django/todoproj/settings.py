8cfad676a0564ddd9d2a503e061c98d46986802e
gprodan <gprodan@univ-ovidius.ro>
2020-09-30 11:48:04 +0300

    v1.10

--- django/todoproj/settings.py
+++ django/todoproj/settings.py
@@ -12,11 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
-# Imports for JWT authentication.
-import json
-from six.moves.urllib import request
-from cryptography.x509 import load_pem_x509_certificate
-from cryptography.hazmat.backends import default_backend
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
